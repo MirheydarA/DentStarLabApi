afb22fa48e84795f80233de49d847235125f4dc1
Mirheyder Alizade <a.mirheyder004@gmail.com>
2026-08-26 09:29:42 +0400

    Nulable warnings

--- DentStarLab.Application/DTOs/Doctors/DoctorDto.cs
+++ DentStarLab.Application/DTOs/Doctors/DoctorDto.cs
@@ -3,7 +3,7 @@ public class DoctorDto
 {
     public int Id { get; set; }
     public string Name { get; set; } = null!;
-    public string Surname { get; set; } = null!;
+    public string? Surname { get; set; } = null!;
     public string? Phone { get; set; }
     public string? Email { get; set; }
     public bool IsActive { get; set; }
